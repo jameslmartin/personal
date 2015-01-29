81c1ac412240c39831b916aaba99658dd9ec87f4
James Martin <jamesml@live.unc.edu>
2015-01-28 21:39:21 -0500

    Initial commit to overhaul locally.

---
+++ net-score/NCServers.php
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<settings>
+<server url="http://speedtest.carolinanet.com/speedtest/upload.php" lat="36.0799" lon="-79.8194" name="Greensboro, NC" country="United States" cc="US" sponsor="CAROLINANET" id="896"  url2="http://66.248.128.86/speedtest/upload.php" host="speedtest.carolinanet.com:8080" />
+<server url="http://speedtest.oit.duke.edu/speedtest/upload.php" lat="35.9886" lon="-78.9072" name="Durham, NC" country="United States" cc="US" sponsor="Duke University" id="4185"  />
+<server url="http://speedtest31.suddenlink.net/speedtest/upload.php" lat="35.9417" lon="-77.7972" name="Rocky Mount, NC" country="United States" cc="US" sponsor="Suddenlink Communications LLC" id="5401"  url2="http://speedtest32.suddenlink.net/speedtest/upload.php" host="speedtest31.suddenlink.net:8080" />
+<server url="http://speedtest.ccvn.com/speedtest/upload.asp" lat="35.9186" lon="-82.2975" name="Burnsville, NC" country="United States" cc="US" sponsor="Country Cablevision, Inc." id="4905"  url2="http://192.34.128.72/speedtest/speedtest/upload.asp" host="speedtest.ccvn.com:8080" />
+<server url="http://speedtest.us2.layeredhosts.com/speedtest/upload.php" lat="35.9083" lon="-81.5300" name="Lenoir, NC" country="United States" cc="US" sponsor="LayeredServer" id="4736"  url2="http://dacentec.layeredhosts.com/speedtest/mini/speedtest/upload.php" />
+<server url="http://speedtest.ral.tqhosting.com/speedtest/upload.php" lat="35.8189" lon="-78.6447" name="Raleigh, NC" country="United States" cc="US" sponsor="Tranquil Hosting" id="544"  url2="http://speedtest2.tqhosting.com/speedtest/upload.php" host="speedtest.ral.tqhosting.com:8080" bigsamples="0" />
+<server url="http://glspeedtest.myglnc.com/speedtest/upload.aspx" lat="35.7311" lon="-77.9235" name="Wilson, NC" country="United States" cc="US" sponsor="greenlight" id="1191"  host="speedtest.myglnc.com:8080" />
+<server url="http://speedtest.main.nc.us/speedtest/upload.php" lat="35.6008" lon="-82.5542" name="Asheville, NC" country="United States" cc="US" sponsor="Mountain Area Information Network" id="3248"  host="speedtest.main.nc.us:8080" />
+<server url="http://24.224.127.172/speedtest/speedtest/upload.php" lat="35.5847" lon="-80.8103" name="Mooresville, NC" country="United States" cc="US" sponsor="MI CONNECTION" id="3383"  url2="http://24.224.127.172/speedtest2/speedtest/upload.php" host="24.224.127.172:8080" />
+<server url="http://208.54.44.70/speedtest/upload.jsp" lat="35.2269" lon="-80.8433" name="Charlotte, NC" country="United States" cc="US" sponsor="T-Mobile" id="1871"  host="208.54.44.70:8080" />
+<server url="http://clt.speedtest.sbcglobal.net/speedtest/upload.php" lat="35.2269" lon="-80.8433" name="Charlotte, NC" country="United States" cc="US" sponsor="AT&amp;T" id="5028"  host="clt.speedtest.sbcglobal.net:8080" />
+<server url="http://speedtest-clt.spiritcom.com/speedtest/upload.php" lat="35.2269" lon="-80.8433" name="Charlotte, NC" country="United States" cc="US" sponsor="Spirit Communications" id="4211"  url2="http://165.166.8.60/speedtest/upload.php" host="speedtest-clt.spiritcom.com:8080" />
+<server url="http://speedtest.port1.highlandscablegroup.com/speedtest/upload.php" lat="35.0751" lon="-83.2101" name="Highlands, NC" country="United States" cc="US" sponsor="Highlands Cable" id="3046"  url2="http://speedtest.port2.highlandscablegroup.com/speedtest/upload.php" />
+<server url="http://speedtest29.suddenlink.net/speedtest/upload.php" lat="34.8525" lon="-82.3942" name="Greenville, NC" country="United States" cc="US" sponsor="Suddenlink Communications LLC" id="5400"  url2="http://speedtest30.suddenlink.net/speedtest/upload.php" host="speedtest29.suddenlink.net:8080" />
+<server url="http://25.24.5.25/speedtest/upload.php" lat="38.0000" lon="-97.0000" name="Raleigh, NC" country="United States" cc="US" sponsor="Time Warner Cable" id="999" url2="http://24.25.5.25/speedtest/upload.php" host="24.25.5.25:8080" />
+</servers>
+</settings>
